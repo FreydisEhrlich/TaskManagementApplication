4196485edcea8d47d190058df00b929ad62e3130
freyd <freydis.ehrlich@gmail.com>
2023-10-22 11:35:29 +0300

    initial model classes

--- src/main/java/com/example/TaskManagementApplication/Model/Todo.java
+++ src/main/java/com/example/TaskManagementApplication/Model/Todo.java
@@ -1,4 +1,26 @@
 package com.example.TaskManagementApplication.Model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@ToString
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table
 public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(nullable = false)
+    private String title;
+
+    private boolean completed;
+    private LocalDate deadline;
+
 }
--- src/main/java/com/example/TaskManagementApplication/Model/User.java
+++ src/main/java/com/example/TaskManagementApplication/Model/User.java
@@ -1,4 +1,31 @@
 package com.example.TaskManagementApplication.Model;
 
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.*;
+
+@ToString
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @Email
+    @Column(nullable = false, unique = true)
+    private String email;
+    private String password;
+
+    public User(String name, String email, String password){
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
 }
